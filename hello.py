e9a67ebcfb096b1b5faa08f7ba20d5e9fc88883f
Jack Harter <jack.harter@me.com>
2019-08-23 12:09:21 -0500

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,19 @@
+#this program says hello and asks for a name
+
+print('shalom / aloha / gute tag') #say hello
+
+print ('what is your name?') #ask the user for input
+
+myName = input()
+
+print ('it is good to meet you, ' + myName) #greet the user
+
+print ('the length of your name is:') 
+
+print (len(myName)) #calculate the length of their name
+
+print ('what is your age?')
+
+myAge = input() #get their age
+
+print ('You will be ' + str(int(myAge) +1 ) + ' in a year.') #change their input and do a trick
