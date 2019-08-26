124b1fb4d663bb62bcf5966d3cd34fae06dc1e0c
Jack Harter <jack.harter@me.com>
2019-08-26 10:17:28 -0500

    updating

---
+++ vampire.py
@@ -0,0 +1,23 @@
+#this program says hello and asks for a name
+
+print ('what is your name?') #ask the user for input
+
+name = str(input())
+
+print ('it is good to meet you, ' + name) #greet the user
+
+if name == 'Alice':
+
+    print ('Hi, Alice')
+
+elif age < 12:
+
+    print ('You are not Alice, kiddo')
+
+elif age > 2000:
+
+    print ('Unlike yourself, Alice is not an immortal, undead vampire.')
+
+elif age > 100:
+
+    print ('You are not alice, granny')
