7bdd5c13265ec248382cd152ba995748f8b56956
JackHarter <jack.harter@me.com>
2022-03-28 15:32:32 -0500

    fixing collatzSeq.

--- Chapter 3/collatzSequence.py
+++ Chapter 3/collatzSequence.py
@@ -3,12 +3,13 @@ def collatz(number):
         print(number // 2)
         return number // 2
     elif number % 2 == 1:
-        print(3 * number + 1)
-        return 3 * number + 1
+        result = 3 * number + 1
+        print(result)
+        return result
 
 n = input("Give me a number:")
 while n != 1:
     try:
-        n = collatz(int)
+        n = collatz(int(n))
     except ValueError:
         print ("Try using an integer instead, kiddo")
